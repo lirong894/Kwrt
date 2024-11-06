412751f9147ede147fd4df44d97fa683748a585c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-07 01:31:46 +0800

    update

--- devices/ipq50xx_arm/diy.sh
+++ devices/ipq50xx_arm/diy.sh
@@ -4,9 +4,9 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/kernel/qca-* package/boot/uboot-envtools package/firmware/ipq-wifi package/firmware/ath11k-firmware package/kernel/mac80211
+rm -rf package/kernel/qca-* package/boot/uboot-envtools package/firmware/ipq-wifi package/firmware/ath11k-firmware
 
-git_clone_path ipq50xx-mainline-kernel-5.15-openwrt-23.05 https://github.com/hzyitc/openwrt-redmi-ax3000 target/linux/ipq50xx package/firmware/ipq-wifi package/firmware/ath11k-firmware package/kernel/mac80211 package/boot/uboot-envtools package/kernel/qca-nss-dp package/kernel/qca-ssdk
+git_clone_path ipq50xx-mainline-kernel-5.15-openwrt-23.05 https://github.com/hzyitc/openwrt-redmi-ax3000 target/linux/ipq50xx package/firmware/ipq-wifi package/firmware/ath11k-firmware package/boot/uboot-envtools package/kernel/qca-nss-dp package/kernel/qca-ssdk
 
 rm -rf devices/common/patches/qca-ssdk.patch
 
--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,14 +4,10 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf target/linux/qualcommax target/linux/generic package/kernel/qca-* package/kernel/mac80211 devices/common/patches/qca-ssdk.patch package/boot/uboot-envtools package/firmware/ipq-wifi
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware/ipq-wifi package/boot/uboot-envtools package/qca
+rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi target/linux/qualcommax/{base-files,files,image,ipq60xx,Makefile}
+git_clone_path master https://github.com/coolsnowwolf/lede package/firmware/ipq-wifi package/boot/uboot-envtools
 
-git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic
-
-git_clone_path c640f7b93736621b4d56627e4f6ab824093f9c3d https://github.com/openwrt/openwrt package/kernel/mac80211
-
-wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.1 -P include/
+git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/qualcommax
 
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
 
