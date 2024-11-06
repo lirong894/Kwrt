ce12310a209ac17e8d554e92e63e11183eac1cd3
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-07 01:39:47 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,12 +4,11 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi target/linux/qualcommax/{base-files,files,image,ipq60xx,Makefile}
+rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi target/linux/qualcommax/{base-files,files,image,ipq60xx}
 git_clone_path master https://github.com/coolsnowwolf/lede package/firmware/ipq-wifi package/boot/uboot-envtools
 
 git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/qualcommax
 
-sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/qualcommax/Makefile
 
 
 
