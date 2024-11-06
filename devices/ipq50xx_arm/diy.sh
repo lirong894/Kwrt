b2b9bf7a141ec4199b9084f20aaa2acec81c5a13
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-06 22:39:08 +0800

    update

--- devices/ipq50xx_arm/diy.sh
+++ devices/ipq50xx_arm/diy.sh
@@ -8,4 +8,6 @@ rm -rf package/kernel/qca-* package/boot/uboot-envtools package/firmware/ipq-wif
 
 git_clone_path ipq50xx-mainline-kernel-5.15-openwrt-23.05 https://github.com/hzyitc/openwrt-redmi-ax3000 target/linux/ipq50xx package/firmware/ipq-wifi package/firmware/ath11k-firmware package/kernel/mac80211 package/boot/uboot-envtools package/kernel/qca-nss-dp package/kernel/qca-ssdk
 
+rm -rf devices/ipq50xx_arm/patches/qca-ssdk.patch
+
 sed -i "s/wpad-basic-wolfssl/wpad-basic-mbedtls/" target/linux/ipq50xx/Makefile
