1d806e28cde3634554b7d2995424e4c59a7653f4
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-09 13:37:02 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,11 +4,9 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/kernel/mac80211 package/firmware/ipq-wifi package/firmware/ath11k-firmware package/boot/uboot-envtools target/linux/qualcommax
+rm -rf package/firmware/ipq-wifi package/firmware/ath11k-firmware package/boot/uboot-envtools target/linux/qualcommax
 
-git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/kernel/mac80211 package/firmware/ipq-wifi package/firmware/ath11k-firmware package/boot/uboot-envtools target/linux/qualcommax
+git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/firmware/ipq-wifi package/firmware/ath11k-firmware package/boot/uboot-envtools target/linux/qualcommax
 
 rm -rf target/linux/qualcommax/patches-6.6/06*-qca-*.patch
-
-
 
