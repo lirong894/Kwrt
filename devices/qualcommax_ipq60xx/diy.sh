a4bbc7e333d2b23b3cef9a8df64d27920154e36d
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-09 10:19:59 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -8,7 +8,7 @@ rm -rf package/kernel/mac80211 package/firmware/ipq-wifi package/firmware/ath11k
 
 git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/kernel/mac80211 package/firmware/ipq-wifi package/firmware/ath11k-firmware package/boot/uboot-envtools target/linux/qualcommax
 
-rm -rf target/linux/qualcommax/patches-6.6/{06*-qca-}
+rm -rf target/linux/qualcommax/patches-6.6/06*-qca-*.patch
 
 
 
